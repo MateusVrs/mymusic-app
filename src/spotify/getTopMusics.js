faaa11e5caf64b0885f2204782dbcef4dd4eb5a9
Mateus Vieira <matucavieira@gmail.com>
2021-11-04 23:18:22 -0300

    regex paths string

--- src/spotify/getTopMusics.js
+++ src/spotify/getTopMusics.js
@@ -3,7 +3,8 @@ import $ from 'jquery';
 const request = require('request');
 
 const getTopMusics = (limit) => {
-    const top50Playlist = "37i9dQZEVXbMDoHDwVN2tF";
+    var top50Playlist = "37i9dQZEVXbMDoHDwVN2tF";
+    //top50Playlist = "37i9dQZEVXbMXbN3EUUhlg";
     limit = limit > 50 ? 50 : limit;
     request.post(authOptions, (error, response, body) => {
         if (!error && response.statusCode === 200) {
@@ -18,13 +19,17 @@ const getTopMusics = (limit) => {
             }).then(dados => {
                 var topMusicsName = [];
                 dados.items.map((item) => {
+                    const artistName = item.track.artists[0].name;
+                    const musicName = item.track.name;
                     let artistObj = {
-                        artistName: item.track.artists[0].name,
-                        artistPath: item.track.artists[0].name,
-                        musicName: item.track.name,
+                        artistName: artistName,
+                        artistPath: artistName.toLowerCase().replace(/[\])}[{(]/g, "").split(" ").join("-"),
+                        musicPath: musicName.toLowerCase().replace(/[\])}[{(]/g, "").split(" ").join("-"),
+                        musicName: musicName,
                         albumImg: item.track.album.images[0].url
                     }
                     topMusicsName.push(artistObj);
+                    return null;
                 });
                 localStorage.setItem("topMusics", JSON.stringify(topMusicsName));
             });
