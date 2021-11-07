e5011b8555e0b801786dcd08eabd18ff5dc3ad97
Mateus Vieira <matucavieira@gmail.com>
2021-11-07 19:07:46 -0300

    add async update ->  home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
-import getTopMusics from "../spotify/getTopMusics"
+import React, { useEffect, useState } from "react";
+import getTopMusics from "../spotify/getTopMusics";
 import MusicList from "./home/MusicList";
+import MainHeader from "../MainHeader";
 
 const Home = () => {
 
-    getTopMusics(20);
-    const topMusicsJSON = JSON.parse(localStorage.getItem("topMusics"));
+    const [topMusicsJSON, setTopMusicsJSON] = useState(null);
 
-    return (
-        <div className="home-container">
-            <section className="home-header">
-                <h1>!MyMusic</h1>
-                <div className="search-container">
-                    <span className="material-icons search-icon">search</span>
-                    <input className="search" type="text" placeholder="Type to search..." maxLength="25" />
-                </div>
-            </section>
-            <section className="home-content">
-                <MusicList JsonInfo={topMusicsJSON} />
-            </section>
-        </div>
-    );
+    useEffect(() => {
+        async function updateTopMusicsJSON() {
+            const returnedTopMusics = await getTopMusics(20);
+            if (returnedTopMusics !== null) {
+                setTopMusicsJSON(returnedTopMusics);
+            }
+        }
+
+        updateTopMusicsJSON();
+    }, []);
+
+    if (topMusicsJSON == null) {
+        return (
+            <div className="home-container">
+            </div>
+        );
+    } else {
+        return (
+            <div className="home-container">
+                <MainHeader />
+                <section className="home-content">
+                    <MusicList JsonInfo={topMusicsJSON} />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Home;
--- src/spotify/getTopMusics.js
+++ src/spotify/getTopMusics.js
@@ -1,40 +1,21 @@
-import authOptions from "./SpotifyAPIConnect";
+import getToken from "./getToken";
 import $ from 'jquery';
-const request = require('request');
 
-const getTopMusics = (limit) => {
+async function getTopMusics(limit) {
     var top50Playlist = "37i9dQZEVXbMDoHDwVN2tF";
-    //top50Playlist = "37i9dQZEVXbMXbN3EUUhlg";
     limit = limit > 50 ? 50 : limit;
-    request.post(authOptions, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            var token = body.access_token;
-            $.ajax({
-                method: "GET",
-                dataType: "Json",
-                url: `https://api.spotify.com/v1/playlists/${top50Playlist}/tracks?market=US&limit=${limit}`,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }).then(dados => {
-                var topMusicsName = [];
-                dados.items.map((item) => {
-                    const artistName = item.track.artists[0].name;
-                    const musicName = item.track.name;
-                    let artistObj = {
-                        artistName: artistName,
-                        artistPath: artistName.toLowerCase().replace(/[\])}[{(]/g, "").split(" ").join("-"),
-                        musicPath: musicName.toLowerCase().replace(/[\])}[{(]/g, "").split(" ").join("-"),
-                        musicName: musicName,
-                        albumImg: item.track.album.images[0].url
-                    }
-                    topMusicsName.push(artistObj);
-                    return null;
-                });
-                localStorage.setItem("topMusics", JSON.stringify(topMusicsName));
-            });
+
+    var response = await $.ajax({
+        method: "GET",
+        dataType: "Json",
+        url: `https://api.spotify.com/v1/playlists/${top50Playlist}/tracks?market=US&limit=${limit}`,
+        headers: {
+            Authorization: `Bearer ${getToken()}`
         }
     });
+
+    var tracksJSON = await response.items;
+    return await tracksJSON;
 }
 
 export default getTopMusics;
