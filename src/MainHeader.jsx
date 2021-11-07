a8124cff8879bce777779e67f509ba08d56a16ff
Mateus Vieira <matucavieira@gmail.com>
2021-11-07 19:17:42 -0300

    refactoring app style

---
+++ src/MainHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainHeader = () => {
+    return (
+        <section className="main-header">
+            <h1>!MyMusic</h1>
+        </section>
+    );
+}
+
+export default MainHeader;
