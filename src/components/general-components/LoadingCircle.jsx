762b2ab6d1aace8a25353b23718081e2054a7779
Mateus Vieira <matucavieira@gmail.com>
2021-11-14 20:48:15 -0300

    loading circle jsx

---
+++ src/components/general-components/LoadingCircle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingCircle = () => {
+    return (
+        <div className="loading-container">
+            <div className="loading-circle"></div>
+        </div>
+    );
+}
+
+export default LoadingCircle;
